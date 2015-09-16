6265c3b20482951a123f5d88447ac3c5042eaa4e
stephds01 <stephds01@gmail.com>
2015-09-16 12:15:33 +0200

    Correction des onglets du menu

--- resources/views/pages/components/menu.blade.php
+++ resources/views/pages/components/menu.blade.php
@@ -2,7 +2,7 @@
     <div class="ent-menu-container">
 
         <ul class="ent-menu">
-            <li><a href="{{ URL::to('/') }}">Histotique des commandes</a></li>
+            <li><a href="{{ URL::to('/') }}">Historique des commandes</a></li>
             <li><a href="{{ URL::to('commandes') }}">Commandes en attente</a></li>
             <li><a href="{{ URL::to('statistiques') }}">Statistiques</a></li>
             <li><a href="{{ URL::to( 'factures') }}">Factures</a></li>
